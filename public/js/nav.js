9c3c15d9874ec02572284dc4169efb93f32d0023
Plakhtiev <monatkotata@gmail.com>
2020-08-10 14:00:56 +0300

    main_page

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,20 +1,23 @@
+
 document.querySelector('.close-nav').onclick = closeNav;
 document.querySelector('.show-nav').onclick = showNav;
 
 function closeNav() {
     document.querySelector('.site-nav').style.left = '-300px';
 }
-
 function showNav() {
     document.querySelector('.site-nav').style.left = '0';
 }
 
 function getCategoryList() {
-    fetch('/get-category-list', {
-        method: 'POST'
-    }).then(function (response) {
+    fetch('/get-category-list',
+        {
+            method: 'POST'
+        }
+    ).then(function (response) {
         return response.text();
-    }).then(function (body) {
+    }
+    ).then(function (body) {
         showCategoryList(JSON.parse(body));
     })
 }
